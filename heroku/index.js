21f85c78e83fa1f389f0aec5e831a855b8ea1a75
camercad <57329246+camercad@users.noreply.github.com>
2019-11-03 17:33:09 -0300

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -13,13 +13,14 @@ var xhub = require('express-x-hub');
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
-app.use(xhub({ algorithm: 'sha1', secret: process.env.76665560936faf26b9e5ae5473f1c905 }));
+
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 
 app.use(bodyParser.json());
 
 
 
-var token = process.env.Servicio || 'token';
+var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
